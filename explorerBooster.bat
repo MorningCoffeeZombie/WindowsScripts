6070af736ef4194472ffca24d20048894ae8fdcc
rmjenson <40343762+rmjenson@users.noreply.github.com>
2018-08-20 08:46:04 -0500

    Create explorerBooster.bat

---
+++ explorerBooster.bat
@@ -0,0 +1,10 @@
+@echo off
+
+
+del "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\Mail Recipient.MAPIMail"
+del "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\SendTo\Fax Recipient.lnk"
+del "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Libraries"
+
+
+::pause
+exit
